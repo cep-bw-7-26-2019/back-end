8c2d76b17805fa5962ed62b2367994e1c02f0589
angel prats <angelpratz@gmail.com>
2019-08-01 15:31:19 -0400

    fixing bugs

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,57 +1,57 @@
-const router = require("express").Router();
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
+const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
-const Users = require("../users/users-model.js");
-const secrets = require("../config/secrets.js");
+const Users = require('../users/users-model.js');
+const secrets = require('../config/secrets.js');
 
-router.post("/register", (req, res) => {
-    let user = req.body;
-    const hash = bcrypt.hashSync(user.password, 10);
-        user.password = hash;
+router.post('/register', (req, res) => {
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 10);
+  user.password = hash;
 
-    Users.add(user)
-        .then(saved => {
-        res.status(201).json(saved);
+  Users.add(user)
+    .then(saved => {
+      res.status(201).json(saved);
     })
     .catch(error => {
-        res.status(500).json(error);
+      res.status(500).json(error);
     });
 });
 
-router.post("/login", (req, res) => {
-    let { username, password } = req.body;
+router.post('/login', (req, res) => {
+  let { username, password } = req.body;
 
-    Users.findBy({ username })
+  Users.findBy({ username })
     .first()
     .then(user => {
-        if (user && bcrypt.compareSync(password, user.password)) {
+      if (user && bcrypt.compareSync(password, user.password)) {
         const token = generateToken(user);
 
         res.status(200).json({
-            message: `Welcome ${user.username}!`,
-            token
+          message: `Welcome ${user.username}!`,
+          token,
         });
-    } else {
-        res.status(401).json({ message: "Invalid Credentials" });
-    }
-})
+      } else {
+        res.status(401).json({ message: 'Invalid Credentials' });
+      }
+    })
     .catch(error => {
-        res.status(500).json(error);
+      res.status(500).json(error);
     });
 });
 
 function generateToken(user) {
-    delete user.password;
-    const jwtPayload = {
+  delete user.password;
+  const jwtPayload = {
     subject: user.id,
-    user
-};
+    user,
+  };
 
-const jwtOptions = {
-    expiresIn: "1d"
-};
-    return jwt.sign(jwtPayload, secrets.jwtSecret, jwtOptions);
+  const jwtOptions = {
+    expiresIn: '1d',
+  };
+  return jwt.sign(jwtPayload, secrets.jwtSecret, jwtOptions);
 }
 
 module.exports = router;
